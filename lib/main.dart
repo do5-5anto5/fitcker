dc25922a9ca0726af032608a0b6cc6b43e7c4611
Jonathan <dos.santos.2k23@gmail.com>
2025-08-11 18:42:30 -0300

    feat: wrap MyApp with ProviderScope at main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:fitcker/utils/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'screens/onboarding_screen.dart';
 
-void main() async {
-  runApp(const MyApp());
+void main() async {  
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
