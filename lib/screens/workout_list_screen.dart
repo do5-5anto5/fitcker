56efe39a9970609bae64f0dac7aff85323c3b5a9
Jonathan <dos.santos.2k23@gmail.com>
2025-08-12 18:21:24 -0300

    feat: consuming state at WorkoutList widget - convert it to a Consumer watching workoutNotifierProvider - list the workouts - give feedback for empty list - allow the wiget to trigger toggleWorkoutStatus() and removeWorkout()

--- lib/screens/workout_list_screen.dart
+++ lib/screens/workout_list_screen.dart
@@ -1,4 +1,6 @@
+import 'package:fitcker/providers/workout/workout_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../enums/workout_type.dart';
 import '../widgets/workout_calendar_graph.dart';
 import '../widgets/workout_form_dialog.dart';
@@ -7,9 +9,7 @@ class WorkoutListScreen extends StatelessWidget {
   const WorkoutListScreen({super.key});
 
   @override
-  Widget build(
-    BuildContext context,
-  ) {
+  Widget build(BuildContext context) {
     return DefaultTabController(
       length: 2,
       child: Scaffold(
@@ -57,37 +57,61 @@ class WorkoutListScreen extends StatelessWidget {
   }
 }
 
-class _WorkoutList extends StatelessWidget {
+class _WorkoutList extends ConsumerWidget {
   final WorkoutType type;
 
   const _WorkoutList({required this.type});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ref) {
+    final unfilteredWorkouts = ref.watch(workoutNotifierProvider);
+    final workouts = unfilteredWorkouts
+        .where((workout) => workout.type == type)
+        .toList();
+
     return ListView.builder(
       padding: const EdgeInsets.all(16),
-      itemCount: 2,
+      itemCount: workouts.length,
       itemBuilder: (context, index) {
+        final workout = workouts[index];
+        if (workouts.isEmpty) {
+          return Center(child: Text('No workouts data'));
+        }
         return Card(
           child: ListTile(
             enabled: false,
-            title: const Text(
-              "workout 1",
+            title: Text(
+              workout.name,
               style: TextStyle(
-                color: Colors.grey,
+                color: workout.isCompleted ? Colors.grey : Colors.white,
+                decoration: workout.isCompleted
+                    ? TextDecoration.lineThrough
+                    : TextDecoration.none,
               ),
             ),
-            subtitle: const Text(
-              '1 sets',
-              style: TextStyle(
-                color: Colors.grey,
-              ),
+            subtitle: Text(
+              '${workout.sets} sets of ${workout.reps} reps',
+              style: TextStyle(color: Colors.grey),
             ),
             trailing: Row(
               mainAxisSize: MainAxisSize.min,
               children: [
-                Checkbox(value: false, onChanged: (_) {}),
-                IconButton(icon: const Icon(Icons.delete), onPressed: () {}),
+                Checkbox(
+                  value: workout.isCompleted,
+                  onChanged: (_) {
+                    ref
+                        .read(workoutNotifierProvider.notifier)
+                        .toggleWorkoutStatus(workout.id);
+                  },
+                ),
+                IconButton(
+                  icon: const Icon(Icons.delete),
+                  onPressed: () {
+                    ref
+                        .read(workoutNotifierProvider.notifier)
+                        .removeWorkout(workout.id);
+                  },
+                ),
               ],
             ),
           ),
