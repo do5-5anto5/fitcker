44cf3482d5de4f137f4cac3a1395c852270b3f15
Jonathan <dos.santos.2k23@gmail.com>
2025-08-14 16:34:00 -0300

    feat: create Quote model

---
+++ lib/models/quote/quote.dart
@@ -0,0 +1,9 @@
+class Quote {
+  final String quote;
+  final String author;
+
+  Quote({required this.quote, required this.author});
+  factory Quote.fromJson(Map<String, dynamic> json) {
+    return Quote(quote: json['quote'], author: json['author']);
+  }
+}
