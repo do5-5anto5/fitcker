8f64ba296159f64cab0366efaabcaa23163ef8a0
Jonathan <dos.santos.2k23@gmail.com>
2025-08-15 19:11:21 -0300

    feat: create auth_provider.dart with Riverpod Annotation

---
+++ lib/providers/auth/auth_provider.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'auth_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$authNotifierHash() => r'46c6136c82cb96cc2e9effc0b14cdea4f9dd2537';
+
+/// See also [AuthNotifier].
+@ProviderFor(AuthNotifier)
+final authNotifierProvider = NotifierProvider<AuthNotifier, User?>.internal(
+  AuthNotifier.new,
+  name: r'authNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$authNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$AuthNotifier = Notifier<User?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
